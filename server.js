bc42df3481c45c88802f3db3fa461ee77f4083b4
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-11-10 03:19:04 +0530

    api

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://cdinventory.netlify.app/"],
      credentials: true
 }))
 app.use('/',router)
